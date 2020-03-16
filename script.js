31f8e0e00674464e8a52c7c1c7414fa9cc59deac
VitalyLazarenko <hristofor.xxxx@gmail.com>
2020-03-16 09:39:08 +0200

    Add & Delete Transactions

--- script.js
+++ script.js
@@ -15,6 +15,35 @@ const dummyTransaction = [
 
 let transactions = dummyTransaction;
 
+// add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
 // Add transaction to DOM list
 function addTransactionDOM(transaction) {
   // get sign
@@ -27,7 +56,9 @@ function addTransactionDOM(transaction) {
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-    </span> <button class="delete-btn">X</button>
+    </span> <button class="delete-btn" onclick="removeTransaction(${
+      transaction.id
+    })">X</button>
   `;
 
   list.appendChild(item);
@@ -54,6 +85,13 @@ function updateValues() {
   money_minus.innerHTML = `$${expense}`;
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  init();
+}
+
 // init app
 function init() {
   list.innerHTML = '';
@@ -63,3 +101,5 @@ function init() {
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
